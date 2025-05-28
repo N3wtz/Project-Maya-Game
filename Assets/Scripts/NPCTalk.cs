315465af5e380f866cd47400ff161eeefce8a0c1
Gagas Pradipta <gagasjatmiko16@gmail.com>
2025-05-28 12:32:17 +0800

    Dialogue System

---
+++ Assets/Scripts/NPCTalk.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTalk : MonoBehaviour
+{
+    public DialogueSO dialogueSO;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (DialogueManager.Instance.isDialogueActive)
+                DialogueManager.Instance.AdvanceDialogue();
+            else
+                DialogueManager.Instance.StartDialogue(dialogueSO);
+        }
+    }
+}
