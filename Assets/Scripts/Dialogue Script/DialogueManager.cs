3a630240e4f0fe60e589c3e3e8dc8024b779bb69
Gagas Pradipta <gagasjatmiko16@gmail.com>
2025-06-17 17:05:41 +0800

    Update Branching Dialogue New Code

--- Assets/Scripts/Dialogue Script/DialogueManager.cs
+++ Assets/Scripts/Dialogue Script/DialogueManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class DialogueManager : MonoBehaviour
     public CanvasGroup canvasGroup;
     public TMP_Text actorName;
     public TMP_Text dialogueText;
+    public Button[] choiceButtons;
 
     public bool isDialogueActive;
 
@@ -42,6 +44,9 @@ public class DialogueManager : MonoBehaviour
         canvasGroup.alpha = 0;
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
+
+        foreach (var button in choiceButtons)
+            button.gameObject.SetActive(false);
     }
 
     public void StartDialogue(DialogueSO dialogueSO)
@@ -68,7 +73,7 @@ public class DialogueManager : MonoBehaviour
         }
         else
         {
-            EndDialogue();
+            ShowChoices();
         }
     }
 
@@ -128,16 +133,61 @@ public class DialogueManager : MonoBehaviour
         typingCoroutine = null;
     }
 
+    private void ShowChoices()
+    {
+        ClearChoices();
+
+        if(currentDialogue.options.Length > 0)
+        {
+            for (int i = 0; i < currentDialogue.options.Length; i++)
+            {
+                var option = currentDialogue.options[i];
+
+                choiceButtons[i].GetComponentInChildren<TMP_Text>().text = option.optionText;
+                choiceButtons[i].gameObject.SetActive(true);
+
+                choiceButtons[i].onClick.AddListener(() => ChooseOption(option.nextDialogue));
+            }
+        }
+        else
+        {
+            choiceButtons[0].GetComponentInChildren<TMP_Text>().text = "End";
+            choiceButtons[0].onClick.AddListener(EndDialogue);
+            choiceButtons[0].gameObject.SetActive(true);
+        }
+    }
+
+    private void ChooseOption(DialogueSO dialogueSO)
+    {
+        if (dialogueSO == null)
+            EndDialogue();
+        else
+        {
+            ClearChoices();
+            StartDialogue(dialogueSO);
+        }
+    }
+
     private void EndDialogue()
     {
         dialogueIndex = 0;
         isDialogueActive = false;
+        ClearChoices();
 
         canvasGroup.alpha = 0;
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
 
         StartCoroutine(CooldownRoutine());
+    }
+
+    private void ClearChoices()
+    {
+        foreach (var button in choiceButtons)
+        {
+            button.gameObject.SetActive(false);
+            button.onClick.RemoveAllListeners();
+        }
     }
 
     private IEnumerator CooldownRoutine()
